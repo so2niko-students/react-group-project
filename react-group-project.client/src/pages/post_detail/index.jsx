049d74675e3f1661c2adbff77886a6a06aa059de
Olena Thomas <olena.thomas.services@gmail.com>
2024-12-21 19:51:50 -0500

    Added tailwind. Added post.

--- react-group-project.client/src/pages/post_detail/index.jsx
+++ react-group-project.client/src/pages/post_detail/index.jsx
@@ -12,12 +12,13 @@ export default function PostDetail() {
             setItem(data);
         }
         getOnePost();
-    }, []);
+    }, [postID]);
 
-    return (<>
-        <h1></h1>
-        <div>PostDetail</div>
-        
-    </>
+    return (
+        <div className="container mx-auto pt-6">
+            <h1 className="text-3xl">{item.title}</h1>
+            <div className="mb-4 mt-2"><span className="mr-8">{item.authorName}</span><span>{item.dateOfCreation}</span></div>
+            <div>{item.description}</div>
+        </div>
     )
 }
--- react-group-project.client/src/services/post.js
+++ react-group-project.client/src/services/post.js
@@ -1,40 +1,15 @@
 import axios from "axios";
 
-//export async function getPost(id) {
-//    console.log(id);
-//    try {
-//        const postItems = "postItems";
-
-//        const url_postItems = `${postItems}`;
-
-//        const response = await axios.get(url_postItems)
-//        console.log(response.data)
-//        return response.data;
-
-//    } catch (error) {
-//        console.error(error);
-//    }
-//}
-
 export async function getPost(id) {
-    console.log(id);
     try {
         const postItems = "postItems";
+        const url_postItem = `/${postItems}/${id}`;
 
-        const url_postItems = `${postItems}`;
-
-        const response = await axios.get(url_postItems)
+        const response = await axios.get(url_postItem);
         console.log(response.data);
-
         return response.data;
-    }
-    catch (error) {
+
+    } catch (error) {
         console.error(error);
     }
 }
-
-//const response = await axios.get(url_postItems, {
-//    params: {
-//        id: { id }
-//    }
-//})
